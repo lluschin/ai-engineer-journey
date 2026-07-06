557110cb4fd71533ae3d82aade5b6207b8df2a78
Lukas Luschin <lukas.luschin@gmail.com>
2026-07-06 11:53:52 +0200

    finetune context builders parameter

--- src/services/context_builder/context_builder.py
+++ src/services/context_builder/context_builder.py
@@ -1,9 +1,12 @@
+import logging
 from models.chat_models import Source
 
-CONTEXT_BUILDER_VERSION = 1.0
+CONTEXT_BUILDER_VERSION = 1.2
 
 _SCORE_THRESHOLD: float = 0.4
-_MAX_CHARS = 1000
+_MAX_CHARS = 2000
+
+logger = logging.getLogger(__name__)
 
 
 def create_context(context: list[Source]) -> str:
@@ -13,5 +16,8 @@ def create_context(context: list[Source]) -> str:
         if source.score > _SCORE_THRESHOLD and \
             len(context_text) + len(source.document) <= _MAX_CHARS: 
             context_text += f"\n\n{i}: Score{source.score}\n{source.document}"
+        else:
+            logger.debug(f'used first {i} of the context sources.')
+            break
 
     return context_text
